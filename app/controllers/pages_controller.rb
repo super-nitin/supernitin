76029655cd6a1a2738e6b36c09c6f084c84553ad
Nitin Gupta <git@radpid.com>
2010-12-28 01:00:25 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Home"
   end
 
   def contact
-  end
-  
-  def about
+    @title = "Contact"
   end
 
+  def about
+    @title = "About"
+  end
 end
