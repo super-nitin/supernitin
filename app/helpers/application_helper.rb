acf084063a3750ad215dd3a40a0d6b7dcefe041b
Nitin Gupta <git@radpid.com>
2010-12-28 14:08:00 -0500

    Add GA code to application helper - again.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,19 +12,17 @@ module ApplicationHelper
 
 
   def google_analytics_js
-    <script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-8022246-2']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
-
+    '<script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
     </script>
+    <script type="text/javascript">
+    try{ 
+    var pageTracker = _gat._getTracker("UA-8022246-2");
+    pageTracker._trackPageview();
+    } catch(err) {} 
+    </script>'
+
   end
 
 
